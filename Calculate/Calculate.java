db50efa8c7baa150e36a6c94c4280efb4172ad31
Sergey <sergord9@gmail.com>
2020-06-16 23:53:31 +0000

    Upload New File

---
+++ Calculate/Calculate.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.Math;
+
+public class Calculate {
+
+    public static void main(String[] args) {
+        Calculate();
+    }
+    public static void Calculate(){
+
+        float numberA,numberB;
+        double result;
+        String mathAction;
+
+        Scanner scanNumber = new Scanner (System.in);
+        Scanner scanAction = new Scanner (System.in);
+
+        System.out.print("Enter first number:");
+        numberA = scanNumber.nextFloat();
+        System.out.print("Enter math action:");
+        mathAction = scanAction.next();
+        System.out.print("Enter second number:");
+        numberB = scanNumber.nextFloat();
+
+        if(mathAction.equals("+")){
+            result = numberA + numberB;
+            System.out.print(numberA + " " + mathAction + " " + numberB + " = " + result + "\n");
+        }
+        else if(mathAction.equals("-")){
+            result = numberA - numberB;
+            System.out.print(numberA + " " + mathAction + " " + numberB + " = " + result + "\n");
+        }
+        else if(mathAction.equals("*")){
+            result = numberA * numberB;
+            System.out.print(numberA + " " + mathAction + " " + numberB + " = " + result + "\n");
+        }
+        else if(mathAction.equals("/")){
+            result = numberA / numberB;
+            System.out.print(numberA + " " + mathAction + " " + numberB + " = " + result + "\n");
+        }
+        else if(mathAction.equals("^")){
+            result = Math.pow(numberA,numberB);
+            System.out.print(numberA + " " + mathAction + " " + numberB + " = " + result + "\n");
+        }
+        else{
+            System.out.println("Incorrect math action");
+            System.out.print("Repeat? 1(Y)/0(N)");
+            if(scanNumber.nextInt() == 1) Calculate();
+        }
+
+        System.out.print("Repeat? 1(Y)/0(N)");
+        if(scanNumber.nextInt() == 1) Calculate();
+    }
+
+}
